692c1355742fd4f17cacf4f36dc2c366c61be063
Casey Vande Hey <cvandehey@mail.bradley.edu>
2023-04-10 09:40:31 -0500

    Sun Damage

---
+++ Sanguine Feast/Assets/Scripts/Lost Scripts/PlayerHealth.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public bool weak;
+    public bool inShade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weak = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Shade")
+        {
+            inShade = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        if(collision.gameObject.tag == "Shade")
+        {
+            inShade = false;
+        }
+    }
+}
